b82b8ce45e1b92966e8a29ed7ce4369961c4c61d
goredsoxgo <goredsoxgo@c3db6c4b-30ea-7147-9e80-7ad62e03601b>
2011-05-24 21:46:59 +0000

    Template parser now does a final pass over the template to remove/flatten {^loop} and {^if} block tag indents.
    This "smart unindent" feature means that template sources can be indented and readable, while at the same time output will be free of unintentional indenting artifacts.  Have cake, eat too.

--- trunk/test/com/x5/template/ThemeTest.java
+++ trunk/test/com/x5/template/ThemeTest.java
@@ -22,4 +22,12 @@ public class ThemeTest
         
         assertEquals("Override Layer\n", c.toString());
     }
+    
+    @Test
+    public void testUnindenter()
+    {
+        String multilineBlock = "  {^loop}\n    {^if (~cond =~ /\\d{1,3}/)}\n  This\n    {~.else}\n  That\n    {^/if}\n  {^/loop}\n";
+        String clean = TemplateSet.removeBlockTagIndents(multilineBlock);
+        assertEquals(clean, "{^loop}\n{^if (~cond =~ /\\d{1,3}/)}\n  This\n{~.else}\n  That\n{^/if}\n{^/loop}\n");
+    }
 }
