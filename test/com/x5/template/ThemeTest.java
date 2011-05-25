224f7004fd71d6d0446ad4d54ded209fcd6f3e27
Tom McClure <tomj74@users.noreply.github.com>
2011-05-25 04:47:28 +0000

    * Still getting a feel for "right" smart-trim behavior.  Made ^if more consistent with ^loop, smart-trim just trims the first newline, if present.  Updated test suite.

--- test/com/x5/template/ThemeTest.java
+++ test/com/x5/template/ThemeTest.java
@@ -26,7 +26,7 @@ public class ThemeTest
     @Test
     public void testUnindenter()
     {
-        String multilineBlock = "  {^loop}\n    {^if (~cond =~ /\\d{1,3}/)}\n  This\n    {~.else}\n  That\n    {^/if}\n  {^/loop}\n";
+        String multilineBlock = "\t  {^loop}  \t \n\t    {^if (~cond =~ /\\d{1,3}/)}\n  This\n\t    {~.else}\n  That\n\t    {^/if}\n\t  {^/loop}\n";
         String clean = TemplateSet.removeBlockTagIndents(multilineBlock);
         assertEquals(clean, "{^loop}\n{^if (~cond =~ /\\d{1,3}/)}\n  This\n{~.else}\n  That\n{^/if}\n{^/loop}\n");
     }
