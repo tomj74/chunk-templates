df395682b8b80dbfa99fc0fca65cfcdfeddb0e75
Václav Kužel <vkuzel@gmail.com>
2016-01-26 22:56:17 +0100

    Added TemplateSet.classpathThemesFolder field that allows to change default folder name for themes stored on classpath. Extracted ensure trailing directory separator algorithm to utility class.

---
+++ src/main/java/com/x5/util/Path.java
@@ -0,0 +1,14 @@
+package com.x5.util;
+
+public class Path {
+    public static String ensureTrailingSeparator(String path) {
+        if (path != null) {
+            char lastChar = path.charAt(path.length()-1);
+            char fs = System.getProperty("file.separator").charAt(0);
+            if (lastChar != '\\' && lastChar != '/' && lastChar != fs) {
+                return path + fs;
+            }
+        }
+        return path;
+    }
+}
